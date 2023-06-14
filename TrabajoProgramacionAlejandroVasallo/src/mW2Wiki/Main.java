df27beae03982389c5526769036f3311c7180d7d
Alex <alejandrovasallomiguel@gmail.com>
2023-06-14 17:25:49 +0200

    FINAL

--- TrabajoProgramacionAlejandroVasallo/src/mW2Wiki/Main.java
+++ TrabajoProgramacionAlejandroVasallo/src/mW2Wiki/Main.java
@@ -3,11 +3,8 @@ package mW2Wiki;
 import mW2Wiki.MENUSFUSIONADO.MENUFUSIONADO;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		
-		MENUFUSIONADO.main(args);
-		
+		MENUFUSIONADO.main(args);	
 	}
-
 }
